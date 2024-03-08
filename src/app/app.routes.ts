67d37e5f1c188725a9d07e6c6fb2757920f2ba19
jasarevicedis <e.jasarevic03@gmail.com>
2024-03-08 23:36:58 +0100

    add base routes for teacher dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { TeacherDashboardComponent } from './features/teacher-dashboard/teacher-dashboard.component';
+import { TeacherCoursesPageComponent } from './features/teacher-dashboard/pages/teacher-courses-page/teacher-courses-page.component';
+import { TeacherStudentsPageComponent } from './features/teacher-dashboard/pages/teacher-students-page/teacher-students-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'teacher', component: TeacherDashboardComponent, children: [
+        {path: 'courses', component: TeacherCoursesPageComponent},
+        {path: 'students', component: TeacherStudentsPageComponent}
+    ]}
+];
--- src/app/features/teacher-dashboard/teacher-dashboard.component.ts
+++ src/app/features/teacher-dashboard/teacher-dashboard.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { TeacherSidebarComponent } from "./layout/teacher-sidebar/teacher-sidebar.component";
+import { TeacherHeaderComponent } from "./layout/teacher-header/teacher-header.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-teacher-dashboard',
-  standalone: true,
-  imports: [],
-  templateUrl: './teacher-dashboard.component.html',
-  styleUrl: './teacher-dashboard.component.scss'
+    selector: 'app-teacher-dashboard',
+    standalone: true,
+    templateUrl: './teacher-dashboard.component.html',
+    styleUrl: './teacher-dashboard.component.scss',
+    imports: [TeacherSidebarComponent, TeacherHeaderComponent, RouterModule]
 })
 export class TeacherDashboardComponent {
 
